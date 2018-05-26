34346098f70e4a0cae3acea1599f8af48213c8ee
Paul Franco <paulfranco@me.com>
2018-05-25 22:45:55 -0700

    Added FoodTruck Model

--- app/src/main/java/co/paulfran/paulfranco/foodtruck/model/FoodTruck.java
+++ app/src/main/java/co/paulfran/paulfranco/foodtruck/model/FoodTruck.java
@@ -2,6 +2,67 @@ package co.paulfran.paulfranco.foodtruck.model;
 
 public class FoodTruck {
 
+    private String id = "";
+    private String name = "";
+    private String foodType = "";
+    private Double avgCost = 0.0;
+    private Double latitude = 0.0;
+    private Double longitude = 0.0;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public Double getAvgCost() {
+        return avgCost;
+    }
+
+    public void setAvgCost(Double avgCost) {
+        this.avgCost = avgCost;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public FoodTruck(String id, String name, String foodType, Double avgCost, Double latitude, Double longitude) {
+        this.id = id;
+        this.name = name;
+        this.foodType = foodType;
+        this.avgCost = avgCost;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
