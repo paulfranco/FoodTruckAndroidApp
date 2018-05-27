8a00d141b984e9ac5ea9cf2a557d0488116340fe
Paul Franco <paulfranco@me.com>
2018-05-26 23:33:15 -0700

    added dependencies. added FoodTruckHolder and FoodTruckAdapter Classes

---
+++ app/src/main/java/co/paulfran/paulfranco/foodtruck/adapter/FoodTruckAdapter.java
@@ -0,0 +1,16 @@
+package co.paulfran.paulfranco.foodtruck.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import co.paulfran.paulfranco.foodtruck.holder.FoodTruckHolder;
+
+public class FoodTruckAdapter extends RecyclerView.Adapter<FoodTruckHolder>{
+
+    @Override
+    public FoodTruckHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+}
---
+++ app/src/main/java/co/paulfran/paulfranco/foodtruck/holder/FoodTruckHolder.java
@@ -0,0 +1,12 @@
+package co.paulfran.paulfranco.foodtruck.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class FoodTruckHolder extends RecyclerView.ViewHolder {
+
+    public FoodTruckHolder(View itemView) {
+        super(itemView);
+    }
+
+}
