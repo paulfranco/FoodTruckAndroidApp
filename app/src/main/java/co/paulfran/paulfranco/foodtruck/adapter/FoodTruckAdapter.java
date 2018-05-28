67cb9c40417370d0e554ff7b209488f9aacc899d
Paul Franco <paulfranco@me.com>
2018-05-27 23:54:04 -0700

    added recycler and card view

--- app/src/main/java/co/paulfran/paulfranco/foodtruck/adapter/FoodTruckAdapter.java
+++ app/src/main/java/co/paulfran/paulfranco/foodtruck/adapter/FoodTruckAdapter.java
@@ -1,11 +1,24 @@
 package co.paulfran.paulfranco.foodtruck.adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
 import co.paulfran.paulfranco.foodtruck.holder.FoodTruckHolder;
 
 public class FoodTruckAdapter extends RecyclerView.Adapter<FoodTruckHolder>{
+
+    // Handle item on click events
+    @Override
+    public void onBindViewHolder(@NonNull FoodTruckHolder holder, int position) {
+
+    }
+
+    // Returns the number of items on our list
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     @Override
     public FoodTruckHolder onCreateViewHolder(ViewGroup parent, int viewType) {
